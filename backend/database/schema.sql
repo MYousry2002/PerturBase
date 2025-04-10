73e7bcfd1dd83db8b198145dbbe19b7f498b42b6
LukeBerger <lukeberg@bu.edu>
2025-04-10 16:29:54 -0400

    Update schema.sql
    Add autoincrement and changed primary key syntax

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE Experiment (
-    ExpID INT PRIMARY KEY,
+    ExpID INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(128),
     Date DATE,
     Treatment VARCHAR(128),
     Source VARCHAR(128),
-    Publication VARCHAR(128)
-);
+    Publication VARCHAR(128),
+    PRIMARY KEY(ExpID)
+) engine = innodb;
 
 CREATE TABLE ChannelMetaData (
-    CMID INT PRIMARY KEY,
+    CMID INT NOT NULL AUTO_INCREMENT,
     ExpID INT,
     Type ENUM('RNA', 'sgRNA', 'ADT'),
     Ncells INT,
@@ -18,22 +19,24 @@ CREATE TABLE ChannelMetaData (
     Ribo_avg FLOAT,
     nFeature_avg_cite INT,
     nCount_avg_cite INT,
+    PRIMARY KEY(CMID)
     FOREIGN KEY (ExpID) REFERENCES Experiment(ExpID)
-);
+) engine = innodb;
 
 CREATE TABLE ChannelCounts (
-    CountID INT PRIMARY KEY,
+    CountID INT NOT NULL AUTO_INCREMENT,
     CMID INT,
     Cell_Name VARCHAR(128),
     Feature VARCHAR(128),
     Count INT,
+    PRIMARY KEY(CountID)
     FOREIGN KEY (CMID) REFERENCES ChannelMetaData(CMID),
-    INDEX idx_feature (Feature)
-);
+) engine = innodb;
 
 CREATE TABLE RawCounts (
-    RawID INT PRIMARY KEY,
+    RawID INT NOT NULL AUTO_INCREMENT,
     ExpID INT,
     Matrix_File VARCHAR(256),
+    PRIMARY KEY(RawID)
     FOREIGN KEY (ExpID) REFERENCES Experiment(ExpID)
-);
+); engine = innodb;
