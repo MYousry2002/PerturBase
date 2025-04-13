8b558d88f6d96aeb014f021590368796c93685e7
myousry2002 <mohamed.y.elsadec@gmail.com>
2025-04-13 09:32:20 -0400

    comment axios code for clarity

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,8 +5,8 @@ const isBioed = window.location.hostname === 'bioed-new.bu.edu';
 
 const api = axios.create({
   baseURL: isBioed
-    ? '/students_25/Team10/PerturBase/main'
-    : '',
+    ? '/students_25/Team10/PerturBase/main' // bioed server
+    : '', // use locally
 });
 
 export default api;
