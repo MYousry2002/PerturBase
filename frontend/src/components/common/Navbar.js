a470cd05131b9eeb964ef8890a047d2b59a01c67
myousry2002 <mohamed.y.elsadec@gmail.com>
2025-04-20 11:46:39 -0400

    link the logo/app name in the Navbar to home page

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -5,10 +5,10 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
+      <Link to="/" className="navbar-logo">
         <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="PerturBase logo" className="logo-image" />
         <span className="logo-text">PerturBase</span>
-      </div>
+      </Link>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
